008b7f58093e037001d3ad6c0c1bbf82929a6426
justin-curtis-lu <juclu@ucdavis.edu>
2021-02-26 13:13:35 -0800

    add layout, routes for class
    build model for classification next

--- app.py
+++ app.py
@@ -5,21 +5,32 @@ import pickle
 app = Flask(__name__)
 
 # Open our pickled model
-model = pickle.load(open('model.pkl', 'rb'))
+LRmodel = pickle.load(open('model.pkl', 'rb'))
 
 # Index Route
 @app.route('/')
+@app.route("/home")
 def home():
     return render_template('index.html')
 
-# Route for prediction
-@app.route('/predict',methods=['POST'])
-def predict():
+@app.route('/LR')
+def LR():
+    return render_template('LR.html')
+
+# Route for tip prediction
+@app.route('/predictLR',methods=['POST'])
+def predictLR():
     features = [int(x) for x in request.form.values()]
     label = [np.array(features)]
-    prediction = model.predict(label)
+    prediction = LRmodel.predict(label)
     output = round(prediction[0], 2)
-    return render_template('index.html', prediction_text='Estimated tip Amount: $ {}'.format(output))
+    return render_template('LR.html', prediction_text='Estimated tip Amount: $ {}'.format(output))
+
+@app.route('/classification')
+def classification():
+    return render_template('class.html')
+
+
 
 if __name__ == "__main__":
     app.run(debug=True)
